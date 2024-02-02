c39c67a5da447ae4071728d1f2ef27b44f4ef37c
Deliaaan <delian.ngtmr@gmail.com>
2024-02-02 13:11:19 -0600

    A ver si funciona el host de github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// import react router dom
+import { BrowserRouter as router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails'
+
 const App = () => {
   return <div className='overflow-hidden'>react app</div>;
 };
